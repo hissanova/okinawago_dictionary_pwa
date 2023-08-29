aae2b9cc30e9a413ec814348786b72603d4ec4f7
hissanova <369bodhisattva@gmail.com>
2023-08-29 12:42:18 +0900

    first step

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,7 +1,45 @@
 import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonSearchbar, IonItem, IonLabel, IonList } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+const okiDict = require('../static/okinawa_01.json');
+const indexToIDDict = okiDict.map((entry: any) => (entry['index'].map((index: string) => [index, entry['id']]))).flat();
+
+function Search() {
+  const [results, setResults] = React.useState([]);
+
+  const handleInput = (ev: Event) => {
+    let query = '';
+    const target = ev.target as HTMLIonSearchbarElement;
+    console.warn(query, target);
+    if (target) query = target.value!;
+    setResults(indexToIDDict.filter((d: any) => d[0].indexOf(query) > -1));
+  };
+
+  return (
+    <>
+      <IonSearchbar
+        debounce={1000}
+        onIonInput={(ev) => handleInput(ev)}
+        animated={true}
+        placeholder="かなで入力"
+      ></IonSearchbar>
+      <Message len={results.length}></Message>
+      <IonList>
+        {results.map((result, objectID) => (
+          <IonItem key={objectID}>{result}</IonItem>
+        ))}
+      </IonList>
+    </>
+  );
+}
+
+type resultsLen = { len: number };
+const Message = ({ len }: resultsLen) => {
+  if (len == 0)
+    return <p>検索ワードを入力して下さい。</p>;
+  else
+    return <p>{len} 件の見出し語がヒットしました。</p>;
+}
 
 const Tab1: React.FC = () => {
   return (
@@ -22,39 +60,5 @@ const Tab1: React.FC = () => {
     </IonPage>
   );
 };
-
-const data = [
-  'なは',
-  'こざ',
-];
-
-function Search() {
-  const [results, setResults] = React.useState([...data]);
-
-  const handleInput = (ev: Event) => {
-    let query = '';
-    const target = ev.target as HTMLIonSearchbarElement;
-    if (target) query = target.value!.toLowerCase();
-
-    setResults(data.filter((d) => d.toLowerCase().indexOf(query) > -1));
-  };
-
-  return (
-    <>
-      <IonSearchbar
-        debounce={1000}
-        onIonInput={(ev) => handleInput(ev)}
-        animated={true}
-        placeholder="かなで入力"
-      ></IonSearchbar>
-      <p>{results.length} 件の見出し語がヒットしました。</p>
-      <IonList>
-        {results.map((result, objectID) => (
-          <IonItem key={objectID}>{result}</IonItem>
-        ))}
-      </IonList>
-    </>
-  );
-}
 
 export default Tab1;
