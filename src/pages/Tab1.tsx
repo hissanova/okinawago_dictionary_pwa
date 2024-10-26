812b633007772c5717364297eedd30e84fe85a03
hissanova <369bodhisattva@gmail.com>
2024-10-26 19:31:41 +0900

    Fetch accordion group event

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,7 +1,7 @@
 import React, { Profiler } from 'react';
 import { Suspense, lazy } from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonSearchbar, IonItem, IonLabel, IonList } from '@ionic/react';
-import { IonAccordion, IonAccordionGroup } from '@ionic/react';
+import { IonAccordion, IonAccordionGroup, AccordionGroupChangeEventDetail, AccordionGroupCustomEvent } from '@ionic/react';
 import { v4 as uuidv4 } from 'uuid';
 import './Tab1.css';
 // import OkiDictEntry from './OkiDictEntry';
@@ -46,36 +46,45 @@ const Message = ({ len }: resultsLen) => {
 }
 
 
-const SearchResults = ({ results, searchTerm }: any) => (
-  <>
-    {
-      results.length > 0 ?
-        <IonAccordionGroup >
-          {results.map((result: [string, any], objectID: number) => (
-            <IonAccordion key={`accordion-${objectID}`} value={`${objectID}`}>
-              <IonItem slot="header" color="light">
-                <IonLabel >
-                  <div>
-                    {
-                      reactStringReplace(result[0], searchTerm, (match: string, i: number) => (
-                        <b key={i}>{match}</b>
-                      ))
-                    }
-                  </div>
-                </IonLabel>
-              </IonItem>
-              <div className="ion-padding" slot="content">
-                <Suspense fallback={<div>Loading...</div>}>
-                  <OkiDictEntry entryID={result[1]} />
-                </Suspense>
-              </div>
-            </IonAccordion>))
-          }
-        </IonAccordionGroup> :
-        <></>
-    }
-  </>
-)
+
+const SearchResults = ({ results, searchTerm }: any) => {
+  const printAccordionState = (ev: Event): void => {
+    const event = ev as AccordionGroupCustomEvent;
+    console.log(event);
+    console.log(event.detail)
+    if (event.detail.value) console.log(results[event.detail.value]);
+  };
+  return (
+    <>
+      {
+        results.length > 0 ?
+          <IonAccordionGroup onIonChange={printAccordionState}>
+            {results.map((result: [string, any], objectID: number) => (
+              <IonAccordion key={`accordion-${objectID}`} value={`${objectID}`}>
+                <IonItem slot="header" color="light">
+                  <IonLabel >
+                    <div>
+                      {
+                        reactStringReplace(result[0], searchTerm, (match: string, i: number) => (
+                          <b key={i}>{match}</b>
+                        ))
+                      }
+                    </div>
+                  </IonLabel>
+                </IonItem>
+                <div className="ion-padding" slot="content">
+                  <Suspense fallback={<div>Loading...</div>}>
+                    <OkiDictEntry entryID={result[1]} />
+                  </Suspense>
+                </div>
+              </IonAccordion>))
+            }
+          </IonAccordionGroup> :
+          <></>
+      }
+    </>
+  );
+}
 
 function onRender(id: string, phase: string, actualDuration: number, baseDuration: number, startTime: number, commitTime: number) {
   console.log(`${id}'s ${phase} phase:`);
@@ -95,6 +104,8 @@ function Tab1() {
     if (target) query = target.value!;
     setResults(query);
   };
+
+
   const results = indexToIDDict.filter((d: any) => d[0].includes(searchTerm || "a"))
     .sort((d1: any, d2: any) => d1[0].indexOf(searchTerm) - d2[0].indexOf(searchTerm));
   console.log(results.length)
